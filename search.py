f420b78cf092823d8c4fc910684131b91e2bdb1c
Judeicca <juderafferty64@gmail.com>
2024-04-06 16:38:42 +0100

    main

--- search.py
+++ search.py
@@ -1,23 +1,63 @@
-#import libaries
-import random
-import numpy as np
-import pandas as pd
-from sklearn.datasets import make_classification
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
+import networkx as nx
+import random
 
-def plot(df: pd.DataFrame, x1: str, x2: str, y: str, title: str = '', save: bool = False, figname='figure.png'):
-    plt.figure(figsize=(14, 7))
-    
-    
-rcParams['axes.spines.top'] = False
-rcParams['axes.spines.right'] = False
+def randomDistance():
+    number = random.randrange(0,20)
+    return number
 
-locations = ["Headquarters", "BranchA", "BranchB", "BranchC", "BranchD", "BranchE", "BranchF", "BranchG", "BranchH", "BranchI", "BranchJ", "BranchK", "BranchL", "BranchM", "BranchN", "BranchO", "BranchP", "BranchQ", "BranchR", "BranchS", "BranchT", "BranchU", "BranchV", "BranchW", "BranchX", "BranchY", "BranchZ"]
+def randomTraffic():
+    traffic = random.randrange(0,5)
+    return traffic
 
-distance = 0.5
+def randomUrgency():
+    urgency = random.randrange(0,5)
+    return urgency
 
-X, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1, random_state=1)
+class CityMap:
+    def __init__(self):
+        self.graph = nx.Graph()
 
-df = pd.concat([pd.DataFrame(X), pd.Series(y)], axis=1)
+    def add_connection(self, node1, node2, distance, traffic, urgency):
+        cost = distance + traffic - urgency
+        self.graph.add_edge(node1, node2, weight=cost)
 
+    def find_shortest_path(self, start):
+        return nx.single_source_dijkstra_path_length(self.graph, start)
+
+    def shortest_path_to(self, start, end):
+        shortest_path = nx.shortest_path(self.graph, start, end)
+        shortest_distance = nx.shortest_path_length(self.graph, start, end)
+        return shortest_path, shortest_distance
+
+
+if __name__ == "__main__":
+    city_map = CityMap()
+
+    print("Connections")
+
+    # Add connections and their associated costs
+    city_map.add_connection("A", "B", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("A", "C", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("B", "C", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("C", "D", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("C", "E", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("D", "E", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("B", "E", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("B", "D", randomDistance(), randomTraffic(), randomUrgency())
+    city_map.add_connection("D", "A", 20, 5, 5)
+
+    start_node = "A"
+    end_node = "D"
+
+    shortest_path, shortest_distance = city_map.shortest_path_to(start_node, end_node)
+    print("Shortest path from", start_node, "to", end_node, ":", shortest_path)
+    print("Shortest distance:", shortest_distance)
+
+    # Visualize the graph
+    pos = nx.spring_layout(city_map.graph)
+    nx.draw(city_map.graph, pos, with_labels=True, node_color="lightblue", node_size=1500, font_size=12, font_weight="bold")
+    labels = nx.get_edge_attributes(city_map.graph, 'weight')
+    nx.draw_networkx_edge_labels(city_map.graph, pos, edge_labels=labels)
+    plt.title("City Map")
+    plt.show()
